c0cd7b7afb0f59dd1473c77b8a4037b7a78011ed
Jouke Waleson <jouke@waleson.com>
2018-12-30 12:22:42 +0100

    next interation

---
+++ src/components/Level.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        This is one level
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Level',
+    data() {
+        return {
+        };
+    },
+};
+</script>
