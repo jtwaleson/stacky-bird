2f080442592b001620963c54681cfa906da65e9c
Jouke Waleson <jouke@waleson.com>
2020-12-29 21:09:21 +0100

    add main menu

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div>Hello!</div>
+    <MainMenu/>
     <Board :cols="7" :rows="7"/>
 </template>
 
 <script>
 import Board from './components/Board.vue'
+import MainMenu from './components/MainMenu.vue'
 
 export default {
     name: 'App',
     components: {
         Board,
+        MainMenu,
     }
 }
 </script>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div>
+            <h2>These are the instructions</h2>
+            <ul>
+                <li>ADD</li>
+                <li>MUL</li>
+                <li><i>SRT</i></li> <!-- not unlocked -->
+            </ul>
+        </div>
+        <div>
+            <h2>These are the levels</h2>
+            <ul>
+                <li>Level 1</li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MainMenu',
+    props: {
+    },
+}
+</script>
+<style>
+</style>
