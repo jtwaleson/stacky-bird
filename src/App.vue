bf4e4890cb5f4cbe51e7eaab974b65ed4e7c01a1
Jouke Waleson <jouke@waleson.com>
2020-12-29 16:12:43 +0100

    start over using vue-cli with vue3

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>Hello!</div>
+    <Board cols="4" rows="4"/>
+</template>
+
+<script>
+import Board from './components/Board.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Board,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Board.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="board">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Board',
+    props: {
+        cols: Number,
+        rows: Number,
+    }
+}
+</script>
+
+<style scoped>
+</style>
