51ffd621b7b32d566b9116c16cf045a6b034c004
Jouke Waleson <jouke@easee.online>
2021-01-02 23:46:48 +0100

    add a level with YOLO

---
+++ src/components/levels/Level0005.vue
@@ -0,0 +1,37 @@
+<template>
+    <Board :cols="7" :rows="7" :gridObjects="gridObjects" :finishLevel="finishLevel"/>
+</template>
+<script>
+import Board from '@/components/Board.vue'
+
+export default {
+    name: 'Level0003',
+    displayName: "Level 0005 - You Only Live Once",
+    description: "In this level we introduce randomness",
+    unlocksLevels: ["Level0006"],
+    unlocksInstructions: ["YOLO"],
+    data() {
+        return {
+            gridObjects: [
+                {x: 1, y: 3, ...this.$store.state.instructions["STRT"]},
+                {x: 3, y: 3, ...this.$store.state.instructions["YOLO"]},
+                {x: 7, y: 3, ...this.$store.state.instructions["FINI"]},
+            ],
+        }
+    },
+    components: {
+        Board
+    },
+    methods: {
+        finishLevel() {
+            this.$store.commit("unlockInstruction", "READ");
+            this.$store.commit("completeLevel", "Level0004");
+            this.$store.commit("unlockLevel", "Level0005");
+            this.$store.commit("openMenu");
+            alert("you won");
+        }
+    },
+}
+</script>
+<style>
+</style>
