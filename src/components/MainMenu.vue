3394a30f38bc866a19f7ca77812c2167dcc09c25
Jouke Waleson <jouke@easee.online>
2021-01-02 20:43:51 +0100

    new-style instructions file
    no UI is present, so no Vue component is necessary

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
-        <div>
+        <div class="instruction-grid-container">
             <h2>These are the instructions</h2>
+            <div class="instruction-grid">
             <template v-for="(instruction, instructionName) in instructions" :key="instructionName">
-                <div v-if="instruction.unlocked" style="color: #000">
-                    <component :is="instruction.component"/>
-                </div>
-                <div v-else style="color: #555">
-                    <component :is="instruction.component"/>
+                <div class="instruction" :class="{unlocked: instruction.unlocked}">
+                    <div>{{ instruction.symbol }}</div>
+                    <div class="code">{{ instructionName }}</div>
                 </div>
             </template>
-
+            </div>
         </div>
         <div>
             <h2>These are the levels</h2>
@@ -31,4 +30,43 @@ export default {
 }
 </script>
 <style>
+.instruction-grid-container {
+    width: fit-content;
+    margin: 0 auto;
+    margin-top: 200px;
+}
+.instruction-grid {
+    display:  grid;
+    border-radius: 6px;
+    grid-gap: 15px;
+    background-color: #bbada0;
+    padding: 15px;
+    grid-template-columns: repeat(5, 107px);
+}
+.instruction {
+    display: grid;
+    border-radius: 3px;
+    width: 107px;
+    height: 107px;
+
+    justify-content: center;
+    align-items: center;
+    background-color: #eee4da;
+    color: #776e65;
+    font-family: "Clear Sans", "Helvetica Neue", Arial, sans-serif;
+    font-size: 55px;
+    z-index: 0;
+    opacity: 30%;
+    user-select: none;
+}
+.instruction.unlocked {
+    opacity: 100%;
+}
+.instruction .code {
+    font-size: 14px;
+    text-align: center;
+    font-weight: bold;
+    color: #555;
+    font-family: "Lucida Sans Typewriter", "Lucida Console", monospace;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,35 +3,12 @@ import App from './App.vue'
 import store from './store'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
+import instructions from './instructions'
 
-const requireInstructionComponent = require.context(
-    // The relative path of the components folder
-    './components/instructions',
-    // Whether or not to look in subfolders
-    false,
-    // The regular expression used to match base component filenames
-    /[A-Z]\w+\.(vue|js)$/
-)
-
-requireInstructionComponent.keys().forEach(fileName => {
-    // Get component config
-    const componentConfig = requireInstructionComponent(fileName)
-
-    // Get PascalCase name of component
-    const componentName = upperFirst(
-        camelCase(
-            // Gets the file name regardless of folder depth
-            fileName
-                .split('/')
-                .pop()
-                .replace(/\.\w+$/, '')
-        )
-    )
-    store.commit('registerInstructionComponent', {
-        instructionName: componentName,
-        instructionComponent: markRaw(componentConfig.default || componentConfig),
-    });
-})
+for (const [instructionName, instruction] of Object.entries(instructions)) {
+    instruction.name = instructionName
+    store.commit('registerInstruction', instruction);
+}
 
 const requireLevelComponent = require.context(
     // The relative path of the components folder
