fe7970c2d8cbb3721a65fe20c55b53ad1def2577
Jouke Waleson <jouke@waleson.com>
2021-01-04 16:33:53 +0100

    factory reset implemented

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,17 +1,16 @@
 <template>
-    <div>
-        <div class="instruction-grid-container fixedwidth">
-            <h1>Stacky Bird</h1>
-            <p>This is Stacky Bird, a game to learn programming as well as a revolutionary programming language by itself.</p>
-            <h2>Levels</h2>
-            <p>Complete these levels to unlock new instructions. Later on, you can come back and improve your solutions! Click a level to start playing.</p>
-            <LevelList hideFinished hideNotReachable/>
-            <h2>Instruction Blocks</h2>
-            <p>The following blocks are available in your solutions. If they are greyed out, you still have to unlock them. Hover over the instructions to see what they do.</p>
-            <InstructionList/>
-            <h2>Completed Levels</h2>
-            <LevelList hideUnfinished hideNotReachable/>
-        </div>
+    <div class="instruction-grid-container fixedwidth">
+        <h1>Stacky Bird</h1>
+        <p>This is Stacky Bird, a game to learn programming as well as a revolutionary programming language by itself.</p>
+        <h2>Levels</h2>
+        <p>Complete these levels to unlock new instructions. Later on, you can come back and improve your solutions! Click a level to start playing.</p>
+        <LevelList hideFinished hideNotReachable/>
+        <h2>Instruction Blocks</h2>
+        <p>The following blocks are available in your solutions. If they are greyed out, you still have to unlock them. Hover over the instructions to see what they do.</p>
+        <InstructionList/>
+        <h2>Completed Levels</h2>
+        <LevelList hideUnfinished hideNotReachable/>
+        <a href="#" style="text-decoration: underline; display: block; margin-top: 200px; color: black;" @click="factoryReset">{{ $t("factory-reset-link") }}</a>
     </div>
 </template>
 <script>
@@ -26,6 +25,15 @@ export default {
         LevelList,
     },
     computed: mapState(['instructions', 'levels']),
+    methods: {
+        factoryReset() {
+            if (confirm(this.$t("factory-reset-alert"))) {
+                alert(this.$t("factory-reset-done"));
+                localStorage.clear();
+                window.location.reload();
+            }
+        },
+    },
 }
 </script>
 <style>
