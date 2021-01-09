847b50ce3aaa32eccb83792e67525518952a3833
Jouke Waleson <jouke@waleson.com>
2021-01-09 16:16:36 +0100

    show a modal when completing a level

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,6 +1,22 @@
 <template>
-    <vue-final-modal v-model="showModal">
-        Modal Content Here
+    <vue-final-modal v-model="showLevelCompletedModal">
+        <div class="levelfinishpopup">
+            <div>
+                <h2><T textKey="You finished the level, great job!"/></h2>
+            </div>
+            <div v-if="unlocksLevels.length > 0">
+                <T textKey="You have unlocked the following levels"/>: <span style="font-weight: bold" v-for="level in unlocksLevels" :key="level">{{ level }} </span>
+            </div>
+            <div v-if="unlocksInstructions.length > 0">
+                <T textKey="You have unlocked the following blocks"/>:
+            </div>
+            <div class="unlocks-instructions">
+                <Instruction v-for="(instructionCode, index) in unlocksInstructions" :key="index" v-bind="$store.state.instructions[instructionCode]"/>
+            </div>
+            <div>
+                <button @click="$router.push({ path: '/' })">OK</button>
+            </div>
+        </div>
     </vue-final-modal>
     <div class="main">
         <div class="instructions1">
@@ -88,7 +104,7 @@ export default {
     data() {
         return {
             birdIsMoving: false,
-            showModal: false,
+            showLevelCompletedModal: false,
             playing: false,
             shouldStopPlaying: false,
             bird: {
@@ -207,7 +223,7 @@ export default {
             this.reset();
         },
         finish() {
-            this.$router.push({ path: "/" });
+            this.showLevelCompletedModal = true;
             this.$store.commit("completeLevel", this.name);
             for (const instructionName of this.unlocksInstructions) {
                 this.$store.commit("unlockInstruction", instructionName);
@@ -215,7 +231,6 @@ export default {
             for (const levelName of this.unlocksLevels) {
                 this.$store.commit("unlockLevel", levelName);
             }
-            alert(this.$tr("Level completed"));
         },
         async play() {
             if (this.playing) {
@@ -301,9 +316,6 @@ export default {
         if (!this.name) {
             return;
         }
-        setTimeout(() => {
-            this.showModal = true;
-        }, 400);
         let placedObjects = JSON.parse(localStorage.getItem(this.name) || "[]");
         // TODO this needs guarding
         for (let placedObject of placedObjects) {
@@ -368,6 +380,18 @@ button {
 }
 .board-menu button.delete {
     margin-right: 55px;
+}
+.levelfinishpopup {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    background: #fff;
+    width: 350px;
+    min-height: 450px;
+    margin: 100px auto;
+    padding: 30px;
+    border-radius: 10px;
+    text-align: center;
 }
 .field {
     display: grid;
@@ -450,5 +474,10 @@ button {
 }
 .field.droppable {
     border: 3px solid blue;
+}
+.unlocks-instructions > .instruction {
+    display: inline-block !important;
+    margin: 5px;
+    opacity: 1.0 !important;
 }
 </style>
