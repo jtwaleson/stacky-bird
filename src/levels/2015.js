3cd048eb9f929aea73dcd4abbead38f52cf666e9
Jouke Waleson <jouke@waleson.com>
2021-01-19 22:50:05 +0100

    add level 2015 and introduce the dump instruction

--- src/levels/2015.js
+++ src/levels/2015.js
@@ -1,35 +1,35 @@
 import instructions from '../instructions.js';
 
 export default {
-    displayName: "Swap",
-    description: "There will be two numbers in the input. Return them in reverse order.",
-    unlocksLevels: ["2015"],
-    unlocksInstructions: ["DUMP"],
+    displayName: "Max",
+    description: "There will be two numbers in the input. Return only the largest number.",
+    unlocksLevels: ["2017"],
+    unlocksInstructions: [],
     gridObjects: [
         {x: 1, y: 1, ...instructions["STRT"]},
-        {x: 4, y: 4, ...instructions["ROT1"]},
+        {x: 4, y: 4, ...instructions["MINU"]},
         {x: 7, y: 7, ...instructions["FINI"]},
     ],
     validation: [
         {
             "input": [1, 2],
-            "finalStack": [1, 2],
+            "finalStack": [2],
         },
         {
             "input": [2, 4],
-            "finalStack": [2, 4],
+            "finalStack": [4],
         },
         {
-            "input": [8, 10],
-            "finalStack": [8, 10],
+            "input": [-8, 10],
+            "finalStack": [10],
         },
         {
             "input": [20, 1],
-            "finalStack": [20, 1],
+            "finalStack": [20],
         },
         {
             "input": [15, -1],
-            "finalStack": [15, -1],
+            "finalStack": [15],
         },
     ],
 }
